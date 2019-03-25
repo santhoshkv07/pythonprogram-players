3ffb3cda74910df4f3538c462fe667903929a6d1
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-03-25 16:41:56 +0530

    Add files via upload

---
+++ summa.py
@@ -0,0 +1,18 @@
+n=int(input("enter the number:"))
+r=0
+while(n):
+    s=n%10
+    r=(r*10)+s
+    n=n//10
+print(r)
+t=0
+r=r//10
+while(r):
+    s=r%10
+    t=t+s
+    r=r//100
+if(t%2!=0):
+    print("O")
+else:
+    print("E")
+
